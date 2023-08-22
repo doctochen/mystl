a39ae6c47071fd4c9f9e13732816d45946c87ce6
doctochen <2104702810@qq.com>
2023-08-22 13:09:44 +0800

    finish reverse_iterator

--- mystl/iterator.h
+++ mystl/iterator.h
@@ -195,6 +195,120 @@ namespace mystl
 			while (n++) --i;
 	}
 
+	//advance 的 random_access_iterator_tag 版本
+	template <class RandomIter, class Distance>
+	void advance_dispatch(RandomIter& i, Distance n, random_access_iterator_tag)
+	{
+		i += n;
+	}
+
+	template <class InputIterator, class Distance>
+	void advance(InputIterator& i, Distance n)
+	{
+		advance_dispatch(i, n, iterator_category(i));
+	}
+
+
+	/*****************************************************************************************/
+	//反向迭代器，使前进为后退， 后退为前进
+	template <class Iterator>
+	class reverse_iterator
+	{
+	private:
+		Iterator current;  //对应的正向迭代器
+
+	public:
+		typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+		typedef typename iterator_traits<Iterator>::value_type        value_type;
+		typedef typename iterator_traits<Iterator>::difference_type   difference_type;
+		typedef typename iterator_traits<Iterator>::pointer           pointer;
+		typedef typename iterator_traits<Iterator>::reference         reference;
+
+		typedef Iterator                                              iterator_type;
+		typedef reverse_iterator<Iterator>                            self;
+
+	public:
+		//构造函数
+		reverse_iterator(){}
+		explicit reverse_iterator(iterator_type i):current(i){}
+		reverse_iterator(const self& rhs): current(rhs.current){}
+
+	public:
+		//取出对应的正向迭代器
+		iterator_type base() const
+		{
+			return current;
+		}
+
+		//重载操作符
+		reference operator*() const
+		{
+			//实际对应正向迭代器的前一个位置
+			auto temp = current;
+			return *--tmp;
+		}
+
+		pointer operator->() const
+		{
+			return &(operator*());
+		}
+
+		//前进（++）变为后退（--）
+		self& operator++()
+		{
+			--current;
+			return *this;
+		}
+
+		self operator++(int)
+		{
+			self tmp = *this;
+			--current;
+			return tmp;
+		}
+
+		// 后退(--)变为前进(++)
+		self& operator--()
+		{
+			++current;
+			return *this;
+		}
+		self operator--(int)
+		{
+			self tmp = *this;
+			++current;
+			return tmp;
+		}
+
+		self& operator+=(difference_type n)
+		{
+			current -= n;
+			return 
+		}
+
+		self operator+ (difference_type n) const
+		{
+			return self(current - n);
+		}
+
+		self& operator-=(difference_type n)
+		{
+			current += n;
+			return *this;
+		}
+		self operator-(difference_type n) const
+		{
+			return self(current + n);
+		}
+
+		reference operator[](difference_type n) const
+		{
+			return *(*this + n);
+		}
+	};
+
+	//重载 operator-
+
 }
 
 
