17da5356d8460ac03e1b0fcd35b0c69064821f1c
doctochen <2104702810@qq.com>
2023-08-25 20:36:25 +0800

    finist swap in util.h

--- mystl/util.h
+++ mystl/util.h
@@ -30,6 +30,30 @@ namespace mystl
 		static_assert(!std::is_lvalue_reference<T>::value, "bad forward");
 		return static_cast<T&&> (arg);
 	}
+
+
+	//swap
+	template <class Tp>
+	void swap(Tp& lhs, Tp& rhs)
+	{
+		auto tmp(mystl::move(lhs));
+		lhs = mystl::move(rhs);
+		rhs = mystl::move(tmp);
+	}
+
+	template <class ForwardIter1, class ForwardIter2>
+	ForwardIter2 swap_range(ForwardIter1 first1, ForwardIter1 last1, ForwardIter2 first2)
+	{
+		for (; first1 != last1; first1++, (void)first2++)
+			mystl::swap(*first, *first2);
+		return first2;
+	}
+
+	template <class Tp, size_t N>
+	void swap(Tp(&a)[N], Tp(&b)[N])
+	{
+		mystl::swap_range(a, a + N, b);
+	}
 }
 
 
